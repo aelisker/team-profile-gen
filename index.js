e172c178946e23ae67ce55c0fa3dde80556851ec
Aaron Lisker <aelisker@gmail.com>
2020-05-13 21:48:15 -0700

    several bugs

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const questions = require('./src/questions');
+// const questions = require('./src/questions');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const generateMarkdown = require('./src/generateMarkdown');
+
+const employeeArray = [];
+
+
 
 // function to write README file
 const writeToFile = data => {
@@ -15,14 +22,147 @@ const writeToFile = data => {
     }
   })
 }
+function addEmployee() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of this employee?',
+      validate: name => {
+        if (name) {
+          return true;
+        } else {
+          console.log ('Employees must have a name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'employeeId',
+      message: 'Please enter the employee\'s ID.',
+      validate: employeeId => {
+        if (!employeeId) {
+          console.log('All employees working here are assigned an ID!');
+          return false;
+        } else if (isNaN(employeeId)) {
+          console.log('Employee IDs must be a number!');
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter the employee\'s email address.',
+      validate: email => {
+        if (!email) {
+          console.log('Employee\'s must have an associated email address!');
+          return false;
+        } else if (!email.includes('@')) {
+          console.log('That doesn\'t look like a valid email address. Please try again!');
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'list',
+      name: 'role',
+      message: 'Please choose the employee\'s role.',
+      choices: ['Manager', 'Engineer', 'Intern'],
+    },
+    // found solution to conditional questions on StackOverflow: https://stackoverflow.com/questions/56412516/conditional-prompt-rendering-in-inquirer
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'What is the office number this employee manages?',
+      when: (answers) => answers.role === 'Manager',
+      validate: officeNumber => {
+        if (!officeNumber) {
+          console.log('All managers manage an office!');
+          return false;
+        } else if (isNaN(officeNumber)) {
+          console.log('This company organizes offices by office number. Please enter a number!');
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'What is this engineer\'s Github username?',
+      when: (answers) => answers.role === 'Engineer',
+      validate: github => {
+        if (!github) {
+          console.log('All engineers are required to have a Github account!');
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'internSchool',
+      message: 'What school does / did this intern attend?',
+      when: (answers) => answers.role === 'Intern',
+      validate: school => {
+        if (!school) {
+          console.log('Interns must have an associated school!');
+          return false;
+        } else if (!isNaN(school)) {
+          console.log('What school is named a number? Try again.');
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddEmployee',
+      message: 'Would you like to add another employee?',
+      default: false
+    }
+  ]);
+}
 
-inquirer.prompt(questions)
-.then(answers => {
-  return generateMarkdown(answers);
-})
-.then(readmeContent => {
-  return writeToFile(readmeContent);
-})
-.catch(err => {
-  console.log(err);
-});
+function appStart() {
+  addEmployee()
+  .then(employee => {
+    let { name, employeeId, email, role, officeNumber, githubUsername, internSchool, confirmAddEmployee } = employee;
+    let employeeObj;
+    if (role === 'Manager') {
+      employeeObj = new Manager (name, employeeId, email, officeNumber);
+      console.log(employeeObj);
+    } else if (role === 'Engineer') {
+      employeeObj = new Engineer (name, employeeId, email, githubUsername);
+      console.log(employeeObj);
+    } else {
+      employeeObj = new Intern (name, employeeId, email, internSchool);
+      console.log(employeeObj);
+    }
+    employeeArray.push(employeeObj);
+    console.log(employeeArray);
+
+    if (confirmAddEmployee) {
+      return appStart();
+    } else {
+      return generateMarkdown(employeeArray);
+    }
+  })
+  .then(readmeContent => {
+    return writeToFile(readmeContent);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
+
+appStart();
--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,70 +1,28 @@
-const tableOfContentsInstall = instruct => {
-  if (!instruct) {
-    return ``;
+const createTeam = function(employeeArray) {
+  let cardsArray = [];
+  for (let i = 0; i < employeeArray.length; i++) {
+    let card = `
+    <div class='card'>
+      <h3>${employeeArray[i].name}</h3>
+      <h3>${ employeeArray[i].id }</h3>
+      <h3>${ employeeArray[i].email }</h3>
+      <h3>${ employeeArray[i].getRole() }</h3>
+      `
+      if (employeeArray[i].getRole() === 'Manager') {
+        `<h3>${ employeeArray[i].getOfficeNumber()}</h3>`
+      } else if (employeeArray[i].getRole() === 'Engineer') {
+        `<h3>${ employeeArray[i].getGithub()}</h3>`
+      } else {
+        `<h3>${ employeeArray[i].getSchool()}</h3>`
+      }
+      cardsArray.push(card);
   }
-  return `* [Installation](#Installation)
-  `
-};
-const installInstructions = instruct => {
-  if (!instruct) {
-    return ``;
-  }
-    return `
-## Installation
-Installation instructions listed below:
-\`\`\`
-${instruct}
-\`\`\`
-  `;
-};
+  return cardsArray;
+}
 
-const tableOfContentsContribute = contributions => {
-  if (!contributions) {
-    return ``;
-  }
-  return `
-* [Contributing](#Contributing)
-  `
-};
-const contributeInstructions = contributions => {
-  if (!contributions) {
-    return ``;
-  }
-  return `
-## Contributing
-${contributions}
-  `;
-};
-
-const tableOfContentsTests = tests => {
-  if (!tests) {
-    return ``;
-  }
-  return `
-* [Tests](#Tests)
-`
-};
-const testInstructions = tests => {
-  if (!tests) {
-    return ``;
-  }
-  return `
-## Tests
-${tests}
-  `;
-};
-
-const emailInfo = emailAddress => {
-  if (!emailAddress) {
-    return `.`;
-  }
-  return ` and via email at [${emailAddress}](mailto:${emailAddress}).`;
-};
-
-module.exports = generatedHtml => {
-  const { installation, contribute, tests, email, ...necessary } = generatedHtml;
-    return `# 
-## Questions
-If you have any questions, you can find me at [${necessary.github}](https://github.com/${necessary.github})${emailInfo(email)}
+module.exports = employeeArray => {
+  const { name, employeeId, email, role, officeNumber, githubUsername, internSchool, confirmAddEmployee } = employeeArray;
+    return `Test
+    ${createTeam(employeeArray)}
     `;
 };
